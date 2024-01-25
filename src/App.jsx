bb512a9d0c35e4e77daefaff0c686fc763f71732
tonmoy-dev <rxdevs100@gmail.com>
2024-01-26 00:33:43 +0600

    header hero-area & footer sections added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeroArea from "./components/HeroArea";
 
 function App() {
-  return <h1 className="text-4xl text-center">Tasker App</h1>;
+  return (
+    <>
+      <Header />
+      <HeroArea />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
